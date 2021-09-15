494cd0ab32402e4d426ef26c206f8ea11800699b
akalee <wooogi123@gmail.com>
2021-09-15 22:37:17 +0900

    설정: 안드로이드 Jetpack compose 웹뷰 기본 설정

---
+++ android/app/src/main/java/com/example/breadmap/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.breadmap
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.breadmap.ui.theme.BreadmapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WebViewScreen(urlToRender = "")
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen(urlToRender: String) {
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(urlToRender)
+        }
+    }, update = {
+        it.loadUrl(urlToRender)
+    })
+}
