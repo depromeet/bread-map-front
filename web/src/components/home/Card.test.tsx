04e66967f0a908c4b30b47c69f46e172068f57f6
김한철 <05.hanch.it@gmail.com>
2021-09-26 20:01:00 +0900

    [#19] 스타일 : 파일 prettier 적용
    코드 줄 바꿈만 변경

--- web/src/components/home/Card.test.tsx
+++ web/src/components/home/Card.test.tsx
@@ -5,13 +5,7 @@ it('home/Card', () => {
   const testTitle = 'test title';
   const testBody = 'test body';
 
-  render(
-    <Card
-      href={''}
-      title={testTitle}
-      body={testBody}
-    />
-  );
+  render(<Card href={''} title={testTitle} body={testBody} />);
 
   expect(screen.getByText(testTitle).tagName).toMatch(/^h2$/i);
   expect(screen.getByText(testBody).tagName).toMatch(/^p$/i);
