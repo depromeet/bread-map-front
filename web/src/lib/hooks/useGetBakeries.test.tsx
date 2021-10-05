784853ad038eae734473cce85d115261cf686060
akalee <wooogi123@gmail.com>
2021-10-05 22:13:24 +0900

    기능: 위치 기반 빵집 fetch api 연결

---
+++ web/src/lib/hooks/useGetBakeries.test.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import * as SWR from 'swr';
+import { render } from '@testing-library/react';
+import { useGetBakeries } from './useGetBakeries';
+
+interface TestComponentProps {
+  latitude: number;
+  longitude: number;
+  range: number;
+}
+
+it('lib/hooks/useGetBakeries', async () => {
+  const TestComponent: React.FC<TestComponentProps> = (props) => {
+    const { data } = useGetBakeries(props);
+
+    return (
+      <ul>
+        {data?.map((entity) => (
+          <li key={entity.bakeryId}>
+            <span>{entity.bakeryId}</span>
+            <span>{entity.bakeryName}</span>
+            <span>{entity.flagsCount}</span>
+            <span>{entity.latitude}</span>
+            <span>{entity.address}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  const spy = jest.spyOn(SWR, 'default');
+  render(<TestComponent latitude={0} longitude={1} range={2} />);
+
+  expect(spy).toHaveBeenCalledWith(
+    ['/bakery', 0, 1, 2],
+    expect.any(Function),
+  );
+});
---
+++ web/src/lib/remotes/fetchBase.ts
@@ -0,0 +1,59 @@
+const arrayTypeGuard = (headers?: HeadersInit): headers is string[][] => {
+  if (Array.isArray(headers)) return true;
+  return false;
+}
+
+const recordTypeGuard = (headers?: HeadersInit): headers is Record<string, string> => {
+  if (typeof headers !== 'object') return false;
+  if (headers.toString() !== '[object Object]') return false;
+  return true;
+}
+
+const headersTypeGuard = (headers?: HeadersInit): headers is Headers => {
+  if (typeof headers !== 'object') return false;
+  if (headers.toString() !== '[object Headers]') return false;
+  return true;
+};
+
+const initHeader = (init?: HeadersInit): HeadersInit | undefined => {
+  if (init === undefined) return undefined;
+
+  const ret: Record<string, string> = {};
+
+  if (arrayTypeGuard(init)) {
+    init.forEach(([key, value]) => {
+      ret[key] = value;
+    });
+  }
+
+  if (recordTypeGuard(init)) {
+    Object.keys(init).forEach((key) => {
+      ret[key] = init[key];
+    });
+  }
+
+  if (headersTypeGuard(init)) {
+    const iter = init.entries();
+
+    while (!iter.next().done) {
+      const [key, value] = iter.next().value;
+      ret[key] = value;
+    }
+  }
+
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+    ret['Authorization'] = 'TEST_TOKEN';
+  }
+
+  return ret;
+}
+
+
+export default function fetchBase(
+  info: RequestInfo,
+  init?: RequestInit | undefined,
+): Promise<Response> {
+  const headers = initHeader(init?.headers);
+
+  return fetch(info, { ...init, headers });
+}
