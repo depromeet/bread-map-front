46b6efc22556db2c815cb5cd25953c91806e29a5
김한철 <05.hanch.it@gmail.com>
2021-10-08 14:48:40 +0900

    [#28] 버그 : 테스트 유틸 추가
    테스트에서 컴포넌트 렌더링시
    theme을 못가져오는 문제 해결

---
+++ web/src/utils/testUtils.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react';
+import { BreadmapThemeProvider as Theme } from '@/styles/BreadMapTheming';
+import { render, RenderOptions } from '@testing-library/react';
+
+const AllTheProviders: React.FC = ({ children }) => {
+  return <Theme>{children}</Theme>;
+};
+
+const customRender = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
