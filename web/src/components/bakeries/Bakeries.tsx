eda632fe9b347a39ad05fabb7a26ef45e8083873
yujin <kimyu28jin@gmail.com>
2021-10-18 11:59:18 +0900

    [#44] 디자인 : 베이커리 리스트 컴포넌트 구현

---
+++ web/src/components/bakeries/Bakeries.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { useGetBakeries } from '@/lib/hooks/useGetBakeries';
+
+const Bakery = () => {
+  return (
+    <BakeryItem>
+      <Image src="https://via.placeholder.com/170" alt="image" />
+      <Info></Info>
+    </BakeryItem>
+  );
+};
+
+const Bakeries = () => {
+  const { data: bakeries, error: err } = useGetBakeries({
+    latitude: 37.5657037,
+    longitude: 126.9746676,
+    range: 10,
+  });
+
+  const error = null;
+
+  return (
+    <>
+      <Title>내 주변 빵집</Title>
+      {error && <div>error</div>}
+      {!bakeries && <div>loading...</div>}
+      {bakeries && (
+        <BakeryList>
+          {bakeries.map((item) => (
+            <Bakery key={item.bakeryId} />
+          ))}
+        </BakeryList>
+      )}
+    </>
+  );
+};
+
+export default Bakeries;
+
+const BakeryItem = styled.li`
+  list-style: none;
+  display: flex;
+  margin-bottom: 8px;
+`;
+
+const Image = styled.img`
+  border-radius: 8px;
+`;
+
+const Info = styled.div`
+  flex: 1;
+`;
+
+const Title = styled.h1``;
+
+const BakeryList = styled.ul`
+  padding: 0;
+`;
