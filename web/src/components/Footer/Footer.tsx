8b787e943b3c4252d6671aaf96148210cfa34386
seokzin <seokzin95@gmail.com>
2021-10-23 02:17:35 +0900

    기능 : a 태그를 Next Link로 교체

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -9,35 +9,46 @@ const Footer: React.FC = () => {
   const [currentPage, setCurrentPage] = useState("");
   
   const handleClick = e => {
+    // e.target을 쓰면 이벤트 캡처링 문제가 생겨서 currentTarget 사용
     setCurrentPage(e.currentTarget.name);
+    console.log(e.currentTarget)
+    console.log(`now : ${currentPage}`)
   };
 
 
   return (
     <Base>
-      <a href="#home" name="home" onClick={handleClick}>
-        <IconBox>
-          <Home name="home" stroke={currentPage === "home" ? activeColor : 'black'} />
-        </IconBox>
-      </a>
+      <Link href="#home">
+        <a name="home" onClick={handleClick}>
+          <IconBox>
+            <Home name="home" stroke={currentPage === "home" ? activeColor : 'black'} />
+          </IconBox>
+        </a>
+      </Link>
 
-      <a href="#compass" name="compass" onClick={handleClick}>
-        <IconBox>
-          <Compass stroke={currentPage === "compass" ? activeColor : 'black'} />
-        </IconBox>
-      </a>
+      <Link href="#compass">
+        <a name="compass" onClick={handleClick}>
+          <IconBox>
+            <Compass stroke={currentPage === "compass" ? activeColor : 'black'} />
+          </IconBox>   
+        </a>
+      </Link>
 
-      <a href="#edit" name="edit" onClick={handleClick} >
-        <IconBox>
-          <Edit stroke={currentPage === "edit" ? activeColor : 'black'} />
-        </IconBox>
-      </a>
+      <Link href="#edit" >
+        <a name="edit" onClick={handleClick}>
+          <IconBox>
+            <Edit stroke={currentPage === "edit" ? activeColor : 'black'} />
+          </IconBox>
+        </a>
+      </Link>
 
-      <a href="#user" name="user" onClick={handleClick}>
-        <IconBox>
-          <User stroke={currentPage === "user" ? activeColor : 'black'}  />
-        </IconBox>
-      </a>
+      <Link href="#user"  >
+        <a name="user" onClick={handleClick}>
+          <IconBox>
+            <User stroke={currentPage === "user" ? activeColor : 'black'}  />
+          </IconBox>
+        </a>
+      </Link>
     </Base>
   );
 };
