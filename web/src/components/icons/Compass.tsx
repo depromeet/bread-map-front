958e8185316d3d0979f4e0141f146fa8be3d9a3b
seokzin <seokzin95@gmail.com>
2021-10-23 02:07:31 +0900

    기능 : SVG 컴포넌트에 current props 부여

--- web/src/components/icons/Compass.tsx
+++ web/src/components/icons/Compass.tsx
@@ -9,8 +9,8 @@ export const Compass: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M14.36 14.36L16.48 8L10.12 10.12L8 16.48L14.36 14.36Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="current" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M14.36 14.36L16.48 8L10.12 10.12L8 16.48L14.36 14.36Z" stroke="current" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 );
 
