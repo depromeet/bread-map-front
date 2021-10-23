6afc7f6273dbab65cf5132689982fb3b1e27e185
HanCh <05.hanch.it@gmail.com>
2021-10-23 16:45:08 +0900

    [#58] 스타일 : figma 기반 global 색상 변경 (#60)
    * [#58] 스타일 : figma 기반 global 색상 변경
    
    * [#58] 스타일 : map 버튼 색상 변경
    
    * [#58] 스타일 : 메인페이지 색상 변경

--- web/src/components/common/DaedongBottomSheet.tsx
+++ web/src/components/common/DaedongBottomSheet.tsx
@@ -4,17 +4,16 @@ import { BottomSheet } from 'react-spring-bottom-sheet';
 import 'react-spring-bottom-sheet/dist/style.css';
 
 const DaedongBottomSheet = styled(BottomSheet)`
-  --rsbs-backdrop-bg: ${({ theme }) => theme.color.background}99;
-  --rsbs-bg: ${({ theme }) => theme.color.background};
-  --rsbs-handle-bg: ${({ theme }) => theme.color.label};
-  /* hsla(0, 0%, 0%, 0.14); */
+  --rsbs-backdrop-bg: rgba(0, 0, 0, 60%);
+  --rsbs-bg: ${({ theme }) => theme.color.white};
+  --rsbs-handle-bg: ${({ theme }) => theme.color.gray300};
   --rsbs-max-w: auto;
   --rsbs-ml: env(safe-area-inset-left);
   --rsbs-mr: env(safe-area-inset-right);
   --rsbs-overlay-rounded: 3rem;
   z-index: 10000;
   > div {
-    box-shadow: 0px -8px 10px lightgrey !important;
+    box-shadow: 0px -4px 10px ${({ theme }) => theme.color.gray700};
     bottom: ${FooterHeight}px !important;
   }
 `;
