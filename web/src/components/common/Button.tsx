266833e71b7a71502325d9d1daf678cd125505b5
김한철 <05.hanch.it@gmail.com>
2021-10-26 19:24:33 +0900

    [#58] 리팩토링 : 버튼 스타일 변경

--- web/src/components/common/Button.tsx
+++ web/src/components/common/Button.tsx
@@ -1,57 +1,52 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   rounded?: boolean;
-  bgColor?: 'none' | 'primary' | 'secondery';
+  type?: 'none' | 'primary' | 'secondery';
   size?: 'big' | 'large' | 'medium' | 'small' | 'tiny';
 }
 
 const Button = ({
-  children,
   rounded = false,
-  bgColor = 'primary',
+  type = 'primary',
   size = 'medium',
   ...props
-}: ButtonProps) => {
-  switch (bgColor) {
-    case 'none':
-      return (
-        <NoneButtonStyle size={size} rounded={rounded} {...props}>
-          {children}
-        </NoneButtonStyle>
-      );
-    case 'secondery':
-      return (
-        <SeconderyButtonStyle size={size} rounded={rounded} {...props}>
-          {children}
-        </SeconderyButtonStyle>
-      );
-    default:
-      return (
-        <PrimaryButtonStyle size={size} rounded={rounded} {...props}>
-          {children}
-        </PrimaryButtonStyle>
-      );
-  }
-};
+}: ButtonProps) => (
+  <ButtonStyle
+    data-size={size}
+    data-rounded={rounded}
+    className={type}
+    {...props}
+  />
+);
 
 export default Button;
 
-const DefaultButtonStyle = styled.button<ButtonProps>`
-  height: ${({ size }) =>
-    size === 'big'
-      ? '52px'
-      : size === 'large'
-      ? '44px'
-      : size === 'medium'
-      ? '40px'
-      : size === 'small'
-      ? '36px'
-      : '32px'};
-  font-size: ${({ size }) =>
-    size === 'big' ? '16px' : size === 'tiny' ? '12px' : '14px'};
-  border-radius: ${({ rounded }) => (rounded ? '1.5rem' : '.4rem')};
+const DefaultButtonStyle = css`
+  font-size: 14px;
+  &[data-size='big'] {
+    height: 52px;
+    font-size: 16px;
+  }
+  &[data-size='large'] {
+    height: 44px;
+  }
+  &[data-size='medium'] {
+    height: 40px;
+  }
+  &[data-size='small'] {
+    height: 36px;
+  }
+  &[data-size='tiny'] {
+    height: 32px;
+    font-size: 12px;
+  }
+  &[data-rounded='true'] {
+    border-radius: 1.5rem;
+  }
+  border-radius: '.4rem';
   font-weight: 700;
   width: 100%;
   padding: 0 15px;
@@ -61,36 +56,40 @@ const DefaultButtonStyle = styled.button<ButtonProps>`
   }
 `;
 
-const PrimaryButtonStyle = styled(DefaultButtonStyle)`
-  background: ${({ theme }) => theme.color.primary500};
-  border: 1px solid ${({ theme }) => theme.color.primary500};
-  color: ${({ theme }) => theme.color.white};
+const ButtonStyle = styled.button`
+  ${DefaultButtonStyle};
 
-  &:disabled {
-    background: ${({ theme }) => theme.color.gray400};
-    border: 1px solid ${({ theme }) => theme.color.gray400};
+  &.primary {
+    background: ${({ theme }) => theme.color.primary500};
+    border: 1px solid ${({ theme }) => theme.color.primary500};
     color: ${({ theme }) => theme.color.white};
+
+    &:disabled {
+      background: ${({ theme }) => theme.color.gray400};
+      border: 1px solid ${({ theme }) => theme.color.gray400};
+      color: ${({ theme }) => theme.color.white};
+    }
   }
-`;
 
-const SeconderyButtonStyle = styled(DefaultButtonStyle)`
-  background: ${({ theme }) => theme.color.primary100};
-  border: 1px solid ${({ theme }) => theme.color.primary100};
-  color: ${({ theme }) => theme.color.primary500};
+  &.secondery {
+    background: ${({ theme }) => theme.color.primary100};
+    border: 1px solid ${({ theme }) => theme.color.primary100};
+    color: ${({ theme }) => theme.color.primary500};
 
-  &:disabled {
-    background: ${({ theme }) => theme.color.gray400};
-    border: 1px solid ${({ theme }) => theme.color.gray400};
-    color: ${({ theme }) => theme.color.white};
+    &:disabled {
+      background: ${({ theme }) => theme.color.gray400};
+      border: 1px solid ${({ theme }) => theme.color.gray400};
+      color: ${({ theme }) => theme.color.white};
+    }
   }
-`;
 
-const NoneButtonStyle = styled(DefaultButtonStyle)`
-  background: ${({ theme }) => theme.color.white};
-  border: 1px solid ${({ theme }) => theme.color.gray300};
-  color: ${({ theme }) => theme.color.gray700};
+  &.none {
+    background: ${({ theme }) => theme.color.white};
+    border: 1px solid ${({ theme }) => theme.color.gray300};
+    color: ${({ theme }) => theme.color.gray700};
 
-  &:disabled {
-    color: ${({ theme }) => theme.color.gray700}4d;
+    &:disabled {
+      color: ${({ theme }) => theme.color.gray700}4d;
+    }
   }
 `;
