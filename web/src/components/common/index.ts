68011c3356577496799f04900dcd6deea491c349
HanCh <05.hanch.it@gmail.com>
2021-10-27 13:45:19 +0900

    [#67] 기능 : 바텀 모달 공통 컴포넌트 구현 (#70)
    * [#67] 리팩토링 : 바텀시트 코드 수정
    
    * [#67] 기능 : 바텀 모달용 컴포넌트 추가

--- web/src/components/common/index.ts
+++ web/src/components/common/index.ts
@@ -1,2 +1,3 @@
-export { default as DaedongBottomSheet } from './DaedongBottomSheet';
+export { default as BottomSheet } from './BottomSheet';
+export { default as BottomModal } from './BottomModal';
 export { default as Button } from './Button';
