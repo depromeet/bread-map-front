daea6753a93d6491edcb1113b4601229a1aee2d8
Seokzin So <seokzin95@gmail.com>
2021-11-08 11:27:39 +0900

    [#75] 탐색 페이지 (공사중) 구현 (#89)
    * wip: Resolve merge conflict
    
    * 진행중: Setting 아이콘 index.tsx에 추가
    
    * 기능 : 페이지 공사중(Construction) SVG 컴포넌트 추가
    
    * 기능 : 탐색 페이지(search) 추가
    
    * 진행중 : 누락된 postcode 라이브러리 추가
    
    * 버그 : Footer 높이 포함 제외
    
    * 진행중 : 임시로 붙인 Footer 제거

---
+++ web/src/pages/search.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Setting, Construction } from '@/components/icons';
+
+const Search = () => {
+  return (
+    <Base>
+      <TopHeader>
+        <Blank />
+        <PageTitle>탐색</PageTitle>
+        <Setting />
+      </TopHeader>
+
+      <Layout>
+        <Content>
+          <Construction />
+          <Title>대빵이는 준비중!</Title>
+          <Description>
+            앗, 아직 없는 페이지예요!
+            <br />
+            대동빵팀이 열심히 만들고 있어요!
+            <br />
+            조금만 기다려주세요!
+          </Description>
+        </Content>
+      </Layout>
+    </Base>
+  );
+};
+
+export default Search;
+
+const Base = styled.div`
+  height: calc(100vh - ${({ theme }) => theme.height.footer}px);
+  display: flex;
+  flex-direction: column;
+`;
+
+const Blank = styled.div`
+  width: 24px;
+`;
+
+const PageTitle = styled.header`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const TopHeader = styled.div`
+  padding: 16px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  margin-bottom: 16px;
+`;
+
+const Description = styled.p`
+  font-size: 14px;
+  color: #c4c4c4;
+  line-height: 1.3rem;
+`;
