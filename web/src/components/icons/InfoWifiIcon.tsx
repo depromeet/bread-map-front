05cbd912c79b43afe17cc563a922333cb34f89df
김한철 <05.hanch.it@gmail.com>
2021-11-09 23:21:08 +0900

    [#62] 스타일 : 와이파이 아이콘 변경

--- web/src/components/icons/InfoWifiIcon.tsx
+++ web/src/components/icons/InfoWifiIcon.tsx
@@ -2,26 +2,34 @@ import * as React from 'react';
 
 const InfoWifiIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
-    width={48}
-    height={48}
+    width="48"
+    height="48"
     viewBox="0 0 48 48"
+    className="nofill"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <title>Info Wifi Icon</title>
     <path
-      d="M11.143 21.429c7.714-8.572 18-8.572 25.714 0M16.286 26.572c4.629-5.143 10.8-5.143 15.429 0"
-      stroke="#222"
-      strokeWidth={4}
+      d="M11.1426 21.4289C18.8569 12.857 29.1426 12.857 36.857 21.4289"
+      stroke="current"
+      strokeWidth="4"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M24 33a1.286 1.286 0 100-2.572 1.286 1.286 0 000 2.571z"
-      fill="#222"
-      stroke="#222"
-      strokeWidth={3}
+      d="M16.2861 26.572C20.9147 21.4291 27.0862 21.4291 31.7148 26.572"
+      stroke="current"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M24.0006 32.9992C24.7106 32.9992 25.2863 32.4235 25.2863 31.7135C25.2863 31.0034 24.7106 30.4277 24.0006 30.4277C23.2905 30.4277 22.7148 31.0034 22.7148 31.7135C22.7148 32.4235 23.2905 32.9992 24.0006 32.9992Z"
+      fill="current"
+      stroke="current"
+      strokeWidth="3"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
@@ -29,3 +37,6 @@ const InfoWifiIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
 );
 
 export default InfoWifiIcon;
+
+{
+}
