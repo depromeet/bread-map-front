834c4234a225f19e313df1b09b5b5cb506aae01f
김한철 <05.hanch.it@gmail.com>
2021-11-09 19:41:36 +0900

    [#62] 기능 : 버튼 타입 type attribute랑 동작 겹치는 부분 수정
     전체 button component 리팩토링 필요

--- web/src/components/common/Button.tsx
+++ web/src/components/common/Button.tsx
@@ -2,22 +2,29 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
+//TODO button 타입 변경필요
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   rounded?: boolean;
   type?: 'none' | 'primary' | 'secondery';
+  styleType?: 'none' | 'primary' | 'secondery';
   size?: 'big' | 'large' | 'medium' | 'small' | 'tiny';
+  disabled?: boolean;
 }
 
 const Button = ({
   rounded = false,
   type = 'primary',
+  styleType = 'primary',
   size = 'medium',
+  disabled = false,
   ...props
 }: ButtonProps) => (
   <ButtonStyle
     data-size={size}
     data-rounded={rounded}
+    data-type={styleType}
     className={`${type} ${props.className}`}
+    disabled={disabled}
     {...props}
   />
 );
@@ -59,6 +66,7 @@ const defaultButtonStyle = css`
 const ButtonStyle = styled.button`
   ${defaultButtonStyle};
 
+  &[data-type='primary'],
   &.primary {
     background: ${({ theme }) => theme.color.primary500};
     border: 1px solid ${({ theme }) => theme.color.primary500};
@@ -71,6 +79,7 @@ const ButtonStyle = styled.button`
     }
   }
 
+  &[data-type='secondery'],
   &.secondery {
     background: ${({ theme }) => theme.color.primary100};
     border: 1px solid ${({ theme }) => theme.color.primary100};
@@ -83,6 +92,7 @@ const ButtonStyle = styled.button`
     }
   }
 
+  &[data-type='none'],
   &.none {
     background: ${({ theme }) => theme.color.white};
     border: 1px solid ${({ theme }) => theme.color.gray300};
