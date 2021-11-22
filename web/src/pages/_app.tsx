d44df0b52ec86f13a778c5135f19dff30358089e
akalee <wooogi123@gmail.com>
2021-11-22 18:17:21 +0900

    remove comment

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,11 +2,9 @@ import { BreadmapThemeProvider as Theme } from '@/styles/BreadMapTheming';
 import { Provider } from 'jotai';
 import type { AppProps } from 'next/app';
 
-/*
 if (process.env.NODE_ENV === 'development') {
   require('../mocks');
 }
-*/
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
