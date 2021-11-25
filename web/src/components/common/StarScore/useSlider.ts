d7b17d8bc4980036b38a6318e02fc2738835d669
Kimyujin <kimyu28jin@gmail.com>
2021-11-25 16:07:07 +0900

    [#104] 기능: 별점 슬라이드 기능 구현 (#110)
    * [#104] 기능 : 별 슬라이드 기능 추가
    
    * [#104] 스타일 : 관련 파일 위치 수정, 코드 스타일 수정

---
+++ web/src/components/common/StarScore/useSlider.ts
@@ -0,0 +1,143 @@
+import React, { CSSProperties, RefObject } from 'react';
+import { isBrowser, noop, off, on } from '@/utils/slideUtils';
+import useMountedState from './useMountedState';
+import useSetState from './useSetState';
+
+export interface State {
+  isSliding: boolean;
+  value: number;
+  pos: number;
+  length: number;
+}
+
+export interface Options {
+  onScrub: (value: number) => void;
+  onScrubStart: () => void;
+  onScrubStop: (value: number) => void;
+  reverse: boolean;
+  styles: boolean | CSSProperties;
+  vertical?: boolean;
+}
+
+const useSlider = (
+  ref: RefObject<HTMLElement>,
+  options: Partial<Options> = {}
+): State => {
+  const isMounted = useMountedState();
+  const isSliding = React.useRef(false);
+  const valueRef = React.useRef(0);
+  const frame = React.useRef(0);
+  const [state, setState] = useSetState<State>({
+    isSliding: false,
+    value: 0,
+    pos: 0,
+    length: 0,
+  });
+
+  valueRef.current = state.value;
+
+  React.useEffect(() => {
+    if (isBrowser) {
+      const styles = options.styles === undefined ? true : options.styles;
+      const reverse = options.reverse === undefined ? false : options.reverse;
+
+      if (ref.current && styles) {
+        ref.current.style.userSelect = 'none';
+      }
+
+      const startScrubbing = () => {
+        if (!isSliding.current && isMounted()) {
+          (options.onScrubStart || noop)();
+          isSliding.current = true;
+          setState({ isSliding: true });
+          bindEvents();
+        }
+      };
+
+      const stopScrubbing = () => {
+        if (isSliding.current && isMounted()) {
+          (options.onScrubStop || noop)(valueRef.current);
+          isSliding.current = false;
+          setState({ isSliding: false });
+          unbindEvents();
+        }
+      };
+
+      const onMouseDown = (event: MouseEvent) => {
+        startScrubbing();
+        onMouseMove(event);
+      };
+      const onMouseMove = options.vertical
+        ? (event: MouseEvent) => onScrub(event.clientY)
+        : (event: MouseEvent) => onScrub(event.clientX);
+
+      const onTouchStart = (event: TouchEvent) => {
+        startScrubbing();
+        onTouchMove(event);
+      };
+      const onTouchMove = options.vertical
+        ? (event: TouchEvent) => onScrub(event.changedTouches[0].clientY)
+        : (event: TouchEvent) => onScrub(event.changedTouches[0].clientX);
+
+      const bindEvents = () => {
+        on(document, 'mousemove', onMouseMove);
+        on(document, 'mouseup', stopScrubbing);
+
+        on(document, 'touchmove', onTouchMove);
+        on(document, 'touchend', stopScrubbing);
+      };
+
+      const unbindEvents = () => {
+        off(document, 'mousemove', onMouseMove);
+        off(document, 'mouseup', stopScrubbing);
+
+        off(document, 'touchmove', onTouchMove);
+        off(document, 'touchend', stopScrubbing);
+      };
+
+      const onScrub = (clientXY: number) => {
+        cancelAnimationFrame(frame.current);
+
+        frame.current = requestAnimationFrame(() => {
+          if (isMounted() && ref.current) {
+            const rect = ref.current.getBoundingClientRect();
+            const pos = options.vertical ? rect.top : rect.left;
+            setState({ pos });
+            const length = options.vertical ? rect.height : rect.width;
+            setState({ length });
+            if (!length) return;
+
+            let value = (clientXY - pos) / length;
+
+            if (value > 1) value = 1;
+            else if (value < 0) value = 0;
+
+            if (reverse) value = 1 - value;
+
+            setState({
+              value,
+            });
+
+            (options.onScrub || noop)(value);
+          }
+        });
+      };
+
+      on(ref.current, 'mousedown', onMouseDown);
+      on(ref.current, 'touchstart', onTouchStart);
+
+      return () => {
+        off(ref.current, 'mousedown', onMouseDown);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        off(ref.current, 'touchstart', onTouchStart);
+      };
+    } else {
+      return undefined;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref, options.vertical]);
+
+  return state;
+};
+
+export default useSlider;
---
+++ web/src/pages/star.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StarScore from '@/components/common/StarScore';
+
+const Star = () => {
+  return <StarScore />;
+};
+
+export default Star;
