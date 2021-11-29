58b0d1ade9bbc1a136e4bcca9d2489af37bbf964
김한철 <05.hanch.it@gmail.com>
2021-11-29 17:06:03 +0900

    [#117] 스타일: 기존 map 폴더 제거

--- web/src/components/daedong/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as CategoryCard } from './CategoryCard';
-export { default as CategorySide } from './CategorySide';
-export { default as MapButtonsWrap } from './DaedongMap';
-export { default as DaedongBottomSheet } from './DaedongBottomSheet';
-export { default as MapContainer } from './DaedongMap';
