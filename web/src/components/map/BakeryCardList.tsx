ecb88a7a396ef5416872c38b86171ca73c7db419
김한철 <05.hanch.it@gmail.com>
2021-11-30 15:45:38 +0900

    [#117] 기능: 카테고리 필터시 바텀시트 필터링 기능 추가

--- web/src/components/map/BakeryCardList.tsx
+++ web/src/components/map/BakeryCardList.tsx
@@ -4,6 +4,7 @@ import { useAtom } from 'jotai';
 import {
   bottomSheetTypeAtom,
   currentBakeryIdAtom,
+  currentFilterAtom,
   currentRangeBakeriesAtom,
 } from '@/store/map';
 import BakeryInfoCard from './BakeryInfoCard';
@@ -11,11 +12,24 @@ import BakeryInfoCard from './BakeryInfoCard';
 const BakeryCardList: React.FC = () => {
   const [bottomSheetType] = useAtom(bottomSheetTypeAtom);
   const [currentBakeryId] = useAtom(currentBakeryIdAtom);
+  const [breadFilter] = useAtom(currentFilterAtom);
   const [bakeries] = useAtom(currentRangeBakeriesAtom);
 
   const currentBakeryEntity = bakeries?.find(
     (el) => el.bakeryId === currentBakeryId
   );
+
+  const afterFilterBakeries =
+    breadFilter.length > 0
+      ? bakeries?.filter((bakery) => {
+          for (let i = 0; i < breadFilter.length; i++) {
+            if (bakery.breadCategoryList.includes(breadFilter[i])) {
+              return true;
+            }
+          }
+          return false;
+        })
+      : bakeries;
 
   return (
     <Base>
@@ -37,7 +51,7 @@ const BakeryCardList: React.FC = () => {
             <MultipleTitle>내 주변 빵집</MultipleTitle>
             <SortTypeText>거리순</SortTypeText>
           </TitleBox>
-          {bakeries?.map((entity) => (
+          {afterFilterBakeries?.map((entity) => (
             <BakeryInfoCard
               bakeryId={entity.bakeryId}
               title={entity.bakeryName}
