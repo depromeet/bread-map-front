57d3fdfcb5bb4656fcf5d63f3e0fbc41954bd6d9
akalee <wooogi123@gmail.com>
2021-12-02 19:53:55 +0900

    [#127] 삭제된 코드 revert (#128)

---
+++ web/src/components/AddBreadReview/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { atom, useAtom } from 'jotai';
+import { useAtomDevtools } from 'jotai/devtools';
+import { BreadCategory } from '@/constants/breadCategories';
+import MainAdd from './MainAdd';
+
+export interface Review {
+  category: BreadCategory | null;
+  name: string;
+  price: number;
+  text: string;
+  star: number;
+}
+
+export interface BreadsReview {
+  [key: number]: Review;
+}
+
+export type BreadsUpdate = (update: React.SetStateAction<BreadsReview>) => void;
+
+const initialState = {};
+
+const breadsReviewAtom = atom<BreadsReview>(initialState);
+
+const AddBreadReview = () => {
+  const [breadsReview, updateBreadsReview] = useAtom(breadsReviewAtom);
+  useAtomDevtools(breadsReviewAtom);
+
+  return (
+    <>
+      <MainAdd
+        {...{
+          breadsReview,
+          updateBreadsReview,
+        }}
+      />
+    </>
+  );
+};
+
+export default AddBreadReview;
