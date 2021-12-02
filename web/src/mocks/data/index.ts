55618b971022a2522275793b05a030c705e0d04e
김한철 <05.hanch.it@gmail.com>
2021-12-02 12:59:14 +0900

    [#124] 리팩토링: msw 삭제

--- web/src/mocks/data/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MOCK_BAKERY } from './bakery';
-export { default as MOCK_MAP_BAKERY } from './mapBakery';
-export { default as MOCK_BAKERY_MENU } from './bakeryMenu';
-export { default as MOCK_BAKERY_REVIEW } from './bakeryReview';
