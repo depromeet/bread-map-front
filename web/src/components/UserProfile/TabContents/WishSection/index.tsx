7a8741571b53a415b838b5ce761369a882341d4b
seokzin <seokzin95@gmail.com>
2021-12-04 13:33:46 +0900

    진행중 : 빵집정보카드 연결 (API 연동 실패)

--- web/src/components/UserProfile/TabContents/WishSection/index.tsx
+++ web/src/components/UserProfile/TabContents/WishSection/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { useAtom } from 'jotai';
+import { currentBakeryIdAtom, currentRangeBakeriesAtom } from '@/store/map';
 
-const WishSection = () => {
-  return <Container>가보고 싶어요 컨텐츠</Container>;
+import BakeryInfoCard from '@/components/map/BakeryInfoCard';
+
+const WishSection: React.FC = () => {
+  const [currentBakeryId] = useAtom(currentBakeryIdAtom);
+  const [bakeries] = useAtom(currentRangeBakeriesAtom);
+
+  const currentBakeryEntity = bakeries?.find(
+    (el) => el.bakeryId === currentBakeryId
+  );
+
+  return (
+    <Base>
+      <h1>가보고 싶어요 컨텐츠</h1>
+      {currentBakeryEntity && (
+        <BakeryInfoCard
+          bakeryId={currentBakeryEntity.bakeryId}
+          title={currentBakeryEntity.bakeryName}
+          wentCount={currentBakeryEntity.flagsCount}
+          starAvg={currentBakeryEntity.avgRating}
+          reviewCount={currentBakeryEntity.menuReviewsCount}
+          reviews={currentBakeryEntity.menuReviewList.map(
+            (review) => review.contents
+          )}
+        />
+      )}
+    </Base>
+  );
 };
 
 export default WishSection;
 
-const Container = styled.div``;
+const Base = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
