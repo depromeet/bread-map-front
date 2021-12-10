db8bd64920b1aa6465f31d615f309bdaaab79a3a
seokzin <seokzin95@gmail.com>
2021-12-11 00:56:54 +0900

    기능 : PICKED 상태인 빵집만 호출하도록 filter

--- web/src/components/UserProfile/TabContents/WishSection/index.tsx
+++ web/src/components/UserProfile/TabContents/WishSection/index.tsx
@@ -10,20 +10,24 @@ const WishSection: React.FC = () => {
   if (error) return <div>Error</div>;
   if (!data) return <div>Loading...</div>;
 
+  console.log(data);
+
   return (
     <Base>
-      {data.map((item, idx) => (
-        <BakeryInfoCard
-          key={item.bakeryId}
-          bakeryId={item.bakeryId}
-          title={item.bakeryName}
-          bakeryImage={process.env.NEXT_PUBLIC_S3_URI + item.imgPath}
-          wentCount={item.flagsCount}
-          starAvg={item.avgRating}
-          reviewCount={item.menuReviewsCount}
-          reviews={item.menuReviewContentList}
-        />
-      ))}
+      {data
+        .filter((item) => item.flagType === 'PICKED')
+        .map((item, idx) => (
+          <BakeryInfoCard
+            key={item.bakeryId}
+            bakeryId={item.bakeryId}
+            title={item.bakeryName}
+            bakeryImage={process.env.NEXT_PUBLIC_S3_URI + item.imgPath}
+            wentCount={item.flagsCount}
+            starAvg={item.avgRating}
+            reviewCount={item.menuReviewsCount}
+            reviews={item.menuReviewContentList}
+          />
+        ))}
     </Base>
   );
 };
