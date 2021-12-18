b1e445c6ae8810949322b8b36f0fb97527b90dff
김한철 <05.hanch.it@gmail.com>
2021-12-18 13:51:56 +0900

    리팩토링: 기존 addbread 파일 제거

--- web/src/components/addBread/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AddBreadHeader } from './AddBreadHeader';
-export { default as BreadCategoryList } from './BreadCategoryList';
-export { default as BreadCategorySelect } from './BreadCategorySelect';
-export { default as ReviewForm } from './ReviewForm';
--- web/src/pages/star.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import StarScore from '@/components/common/StarScoreSlider';
-
-const Star = () => {
-  return <StarScore />;
-};
-
-export default Star;
