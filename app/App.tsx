18bff862b599a81b66485071e608e4d481e19c68
김한철 <05.hanch.it@gmail.com>
2022-01-09 16:25:26 +0900

    설정: RN base 설정

--- app/App.tsx
+++ app/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import {View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, { FC } from 'react';
+import { View, Platform } from 'react-native';
+import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Example } from './src/components/example/example';
 
-function App() {
-  return (
-    <>
-      <View style={{flex: 1}}>
-        <MapView
-          nativeID="sad"
-          style={{flex: 1}}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
-    </>
-  );
-}
+const App: FC = () => (
+  <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      provider={Platform.OS === 'ios' ? PROVIDER_DEFAULT : PROVIDER_GOOGLE}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+    <Example start={50} />
+  </View>
+);
 export default App;
---
+++ app/src/utils/bindHook/bindHook.ts
@@ -0,0 +1,37 @@
+import { VFC } from 'react';
+import { createElement, memo } from 'react';
+
+type HookType<HookProps, HookResult> = (props: HookProps) => HookResult;
+
+type Options = {
+  displayName?: string;
+};
+
+/**
+ * 훅과 컴포넌트를 연결시켜주는 유틸입니다.
+ * 컴포넌트에 로직 또는 훅이 있는 경우에 사용하면 됩니다.
+ * 컴포넌트는 단순 데이터를 받아 보여주는 용도로만 사용해주세요.
+ * @param useHook
+ * @param ViewComponent
+ * @param options
+ * @returns 훅의 결과데이터가 바인딩된 컴포넌트
+ */
+export const bindHook = <HookProps extends Record<string, unknown>, HookResult>(
+  useHook: HookType<HookProps, HookResult>,
+  ViewComponent: VFC<HookResult>,
+  options?: Options,
+) => {
+  const { displayName } = options ?? {};
+
+  const MemoizedViewComponent = memo(ViewComponent) as unknown as VFC<HookResult>;
+
+  const Component = memo<HookProps>((props) =>
+    createElement(MemoizedViewComponent, useHook(props)),
+  ) as unknown as VFC<HookProps>;
+
+  if (displayName) {
+    Component.displayName = displayName;
+  }
+
+  return Component;
+};
